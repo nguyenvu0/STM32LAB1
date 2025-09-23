3ac52e880a22d9fa5577afd9af94fd0fa99a815b
nguyenvu0 <vu.luuvuluung04@hcmut.edu.vn>
2025-09-24 00:29:17 +0700

    init for ex9

--- SOURCE_CODE/Core/Src/main.c
+++ SOURCE_CODE/Core/Src/main.c
@@ -1,35 +1,85 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+void setNumberOnClock(int num);
+void clearNumberOnClock(int num);
+/* USER CODE END PFP */
 
+/* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-// Hàm tắt tất cả LED
-void clearAllClock(void) {
-    HAL_GPIO_WritePin(GPIOA,
-                      CLOCK_1_Pin | CLOCK_2_Pin | CLOCK_3_Pin | CLOCK_4_Pin |
-                      CLOCK_5_Pin | CLOCK_6_Pin | CLOCK_7_Pin | CLOCK_8_Pin |
-                      CLOCK_9_Pin | CLOCK_10_Pin | CLOCK_11_Pin | CLOCK_12_Pin,
-                      GPIO_PIN_RESET);
-}
+int leds[12] = {
+    CLOCK_1_Pin, CLOCK_2_Pin, CLOCK_3_Pin, CLOCK_4_Pin,
+    CLOCK_5_Pin, CLOCK_6_Pin, CLOCK_7_Pin, CLOCK_8_Pin,
+    CLOCK_9_Pin, CLOCK_10_Pin, CLOCK_11_Pin, CLOCK_12_Pin
+};
 
-// Hàm bật LED theo số num (0-11)
 void setNumberOnClock(int num) {
     if (num < 0 || num > 11) return;
+    HAL_GPIO_WritePin(GPIOA, leds[num], GPIO_PIN_SET);   // bật LED num
+}
 
-    int leds[12] = {
-        CLOCK_1_Pin, CLOCK_2_Pin, CLOCK_3_Pin, CLOCK_4_Pin,
-        CLOCK_5_Pin, CLOCK_6_Pin, CLOCK_7_Pin, CLOCK_8_Pin,
-        CLOCK_9_Pin, CLOCK_10_Pin, CLOCK_11_Pin, CLOCK_12_Pin
-    };
-
-    clearAllClock(); // tắt hết
-    HAL_GPIO_WritePin(GPIOA, leds[num], GPIO_PIN_SET); // bật LED chỉ định
+void clearNumberOnClock(int num) {
+    if (num < 0 || num > 11) return;
+    HAL_GPIO_WritePin(GPIOA, leds[num], GPIO_PIN_RESET); // tắt LED num
 }
 /* USER CODE END 0 */
 
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
+  /* MCU Configuration--------------------------------------------------------*/
+
   HAL_Init();
   SystemClock_Config();
   MX_GPIO_Init();
@@ -40,15 +90,20 @@ int main(void)
   while (1)
   {
     /* USER CODE BEGIN 3 */
+    // Quét LED chạy vòng quanh: bật -> delay -> tắt
     for (int i = 0; i < 12; i++) {
-        setNumberOnClock(i);   // bật LED theo số i
-        HAL_Delay(500);        // giữ sáng 0.5 giây
+      setNumberOnClock(i);       // bật LED i
+      HAL_Delay(300);            // delay 0.3s
+      clearNumberOnClock(i);     // tắt LED i
     }
     /* USER CODE END 3 */
   }
 }
 
-/* ====================== System & GPIO ====================== */
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -59,7 +114,9 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
     Error_Handler();
+  }
 
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
@@ -67,32 +124,49 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
     Error_Handler();
+  }
 }
 
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
-  GPIO_InitStruct.Pin = CLOCK_1_Pin|CLOCK_2_Pin|CLOCK_3_Pin|CLOCK_4_Pin|
-                        CLOCK_5_Pin|CLOCK_6_Pin|CLOCK_7_Pin|CLOCK_8_Pin|
-                        CLOCK_9_Pin|CLOCK_10_Pin|CLOCK_11_Pin|CLOCK_12_Pin;
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, CLOCK_1_Pin|CLOCK_2_Pin|CLOCK_3_Pin|CLOCK_4_Pin
+                          |CLOCK_5_Pin|CLOCK_6_Pin|CLOCK_7_Pin|CLOCK_8_Pin
+                          |CLOCK_9_Pin|CLOCK_10_Pin|CLOCK_11_Pin|CLOCK_12_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : CLOCK_1_Pin ... CLOCK_12_Pin */
+  GPIO_InitStruct.Pin = CLOCK_1_Pin|CLOCK_2_Pin|CLOCK_3_Pin|CLOCK_4_Pin
+                          |CLOCK_5_Pin|CLOCK_6_Pin|CLOCK_7_Pin|CLOCK_8_Pin
+                          |CLOCK_9_Pin|CLOCK_10_Pin|CLOCK_11_Pin|CLOCK_12_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  HAL_GPIO_WritePin(GPIOA, GPIO_InitStruct.Pin, GPIO_PIN_RESET);
 }
+
+/* USER CODE BEGIN 4 */
+/* USER CODE END 4 */
 
 void Error_Handler(void)
 {
   __disable_irq();
   while (1) {}
 }
-
 
 
 #ifdef  USE_FULL_ASSERT
